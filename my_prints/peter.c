8f16cb462854008760fbd0e3df3b049fe7a9b6d1
Muhammed Abraham Amanokhai <drabraham2024@gmail.com>
2023-02-25 23:15:43 -0800

    my first practice

---
+++ my_prints/peter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Main function entry point
+ * Description: prints "The new president of Nigeria"
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Peter Obi is the new president of Nigeria");
+	puts("\"I love my president");
+	return (0);
+}
