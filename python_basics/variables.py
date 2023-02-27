2bee86cd8398620874cd4cb00881f60596dad668
Muhammed Abraham Amanokhai <drabraham2024@gmail.com>
2023-02-26 18:31:12 -0800

    learning variables

---
+++ python_basics/variables.py
@@ -0,0 +1,10 @@
+#program to show variables, this is a comment to explain the program
+character_name = "Abraham"
+character_age = "25"
+print("There was a man named " + character_name + ",")
+print("He was " + character_age + " years old")
+#user can input any value for name and age and run code
+character_name = "Osagie"
+character_age = "30"
+print("He had a friend named " + character_name + ",")
+print("He just clocked " + character_age + " last year")
