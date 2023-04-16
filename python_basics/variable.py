bedf29585813e6ba0142327e7d35338b4760fce9
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-16 14:10:15 +0100

    python basics

---
+++ python_basics/variable.py
@@ -0,0 +1,51 @@
+x = 5
+j = 10
+z = "good morning"
+print(x)
+print(j)
+print(z)
+# you dont need to define data types in python 
+y = 15
+y = "age"
+print(y)
+#you can define data types using casting
+a = int(3)
+b = float(3.14)
+c = str("john")
+print(a)
+print(b)
+print(c)
+#get the type of a variable
+n = 'book'
+k = 3.14
+o = 4
+print(type(n))
+print(type(k))
+print(type(o))
+# prints the same value for each variable
+x =y = z = 13
+print(x)
+print(y)
+print(z)
+#python allows you to get the value of a variable in a pack
+odd_Numbers = ["1", "3", "9", "11"]
+p, q ,r, s = odd_Numbers
+
+print(p)
+print(q)
+print(r)
+print(s)
+#prints multiple statements seperate variables by ,
+m = "Abraham"
+i = "is"
+l = "good"
+print (m, i, l)
+#prints global variables
+x = "awesome"
+
+def mynum():
+      print("Python is " + x)
+
+      mynum()
+
+          
