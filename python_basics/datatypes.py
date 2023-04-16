091d38f306e5a85c90dcc61e9a519e0acdd700e5
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-17 00:25:26 +0100

    my_commit

---
+++ python_basics/datatypes.py
@@ -0,0 +1,35 @@
+# x is an integer
+x = 10
+print(type(x))
+# a is a string
+a = "john"
+print(type(a))
+# b is a float
+b = 3.14
+z = 5e3
+print(z)
+print(type(b))
+# c is a set
+c = {"orange", "banana", "apple"}
+print(type(c))
+# d is a complex variable
+d = 5j + 3
+print(type(d))
+print(d)
+# e is a dict
+e = {"state" : "Edo", "city" : "benin" , "number": 12} 
+print(type(e))
+# f is a range
+f = range (6)
+print(type(f))
+g = False
+print(type(g))
+# j is a memoryview
+j = memoryview(bytes(8))
+print(type(j))
+print(j)
+# casting of data types 
+y = str("s1")
+print(y)
+r = float(3.5)
+print(r)
---
+++ python_basics/push.sh
@@ -0,0 +1,7 @@
+git add .
+
+read -p "my commit message is :" my_commit
+
+git commit -m ' my_commit'
+
+git push
