4a44f68b80db24d791dfde3c83fa91f7c2f485f0
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-21 11:12:06 +0100

    my_commit

---
+++ python_basics/strings.py
@@ -0,0 +1,42 @@
+a = "good morning"
+print(a)
+# you can find the position of a character
+b = "hello world"
+print(b[0])
+#print specific characters from their positions
+c = "hello"
+print(c[2:4])
+# to print strings in a loop using for
+for d in "friend":
+     print(d)
+     # to print the length of a string
+     e = "elephant"
+     print(len(e))
+     # to  check for word in a string and if true print a text, there must be a space after an if statement
+     f = "i am a good boy"
+     print("boy" in f)
+     print("girl" in f)
+     if "boy" in f:
+          print("my name is Abraham")
+           # to slice a string from beginning or end
+            #from position 3 to 0
+             g = 'hello world'
+              print(g[:3])
+               #from 3 to end
+                print(g[3:])
+                 #negative slicing
+                  print(g[-5:-2])
+                   # to change a string to uppercase
+                    h = "good day"
+                     print(h.upper())
+                      #to change string to lower
+                       print(h.lower())
+                        # to remove whitespace from beginning to end
+                         i = "    hello, world    "
+                          print(i.strip())
+                           # to replace a text or word in a string
+                            print(i.replace("hello", "good morning"))
+                             # to split a string into substrings
+                              print(i.split(","))
+                               #string concatenation
+                                print(a + " " + b )
