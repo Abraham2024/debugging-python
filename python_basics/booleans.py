38504c5457935931deaf1eaaceebf2e2b201d00f
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-21 18:07:11 +0100

    my_commit

---
+++ python_basics/booleans.py
@@ -0,0 +1,12 @@
+# to check a true or false statement
+print(10 > 9)
+print(10 == 9)
+print(10< 9)
+a = "16"
+b = '33'
+if a < b:
+        print("this is a good day")
+    else:
+            print('i wont be going home')
+                #
+                    print(bool(0))
