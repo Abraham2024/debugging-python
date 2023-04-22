fad7ad18f7ff4821bacb1ec35f9ea01d78f1f94c
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-22 17:54:59 +0100

    my_commit

--- python_basics/booleans.py
+++ python_basics/booleans.py
@@ -1,4 +1,4 @@
-# to check a true or false statement
+eck a true or false statement
 print(10 > 9)
 print(10 == 9)
 print(10< 9)
@@ -7,6 +7,16 @@ b = '33'
 if a < b:
         print("this is a good day")
     else:
-            print('i wont be going home')
-                #
-                    print(bool(0))
+           print('i wont be going home') 
+            # 0, false and empty sets will return false
+            def myFunction() :
+                    return True
+                    if myFunction() :
+                                 print("yes")
+                                     else:
+                                                  print("no")
+                                                  #we can use isistance to check if a datatype is true or false
+                                                  x = 16
+                                                  print(isinstance(x, int))
+                                                  y = "john"
+                                                  print(isinstance(x, float))
