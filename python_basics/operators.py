97778eae240b3e31622651aa583766ba40e74781
drabraham2024 <122816848+drabraham2024@users.noreply.github.com>
2023-04-23 20:49:10 +0100

    my_commit

---
+++ python_basics/operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b) #exponential
+print( a % b) # gives the remainder after division
+print(a // b) # gives the answer to the whole number
+x = 3
+if x > 5 and x < 9:
+     print("joy is coming")
+ else:
+         print("good morning")
